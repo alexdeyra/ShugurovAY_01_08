4f0ab50477fdd7633db8f94348e516dcc093e748
alexdeyra <84706962+alexdeyra@users.noreply.github.com>
2025-07-02 14:42:22 +0500

    Add files via upload

---
+++ HumanQualityCalculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HumanQualityCalculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HumanQualityCalculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3402435-a6b7-40c3-a019-fca63f3f0808")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HumanQualityCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ChelovekTests
+{
+    [TestMethod]
+    public void Calculate_ForStandardChelovek_ReturnsCorrectValue()
+    {
+      
+        var chelovek = new Program(175, 30, 70);
+        double expected = (175 * 1.8) - (30 * 4.7) + (70 * 9.6) + 1255;
+        
+        
+        double actual = chelovek.CalculateQ();
+        
+     
+        Assert.AreEqual(expected, actual, 0.01);
+    }
+    
+    [TestMethod]
+    public void Calculate_ForFitGoal_Subtracts600()
+    {
+        
+        var chelovek = new Chelovekgoal(175, 30, 70, "fit");
+        double baseQ = (175 * 1.8) - (30 * 4.7) + (70 * 9.6) + 1255;
+        double expected = baseQ - 600;
+       
+        double actual = chelovek.CalculateQ();
+        
+       
+        Assert.AreEqual(expected, actual, 0.01);
+    }
+    
+    [TestMethod]
+    public void AllChelovek_AddsNewChelovek_CountIncreases()
+    {
+       
+        int initialCount = Program.AllChelovek.Count;
+        
+      
+        var chelovek = new Program(180, 40, 80);
+        int newCount = Program.AllChelovek.Count;
+       
+        Assert.AreEqual(initialCount + 1, newCount);
+    }
+}
+
