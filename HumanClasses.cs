b27ecab4dbc3f9af6caddc4fabc9db192b6840eb
Fretoss <valiev10266@gmail.com>
2025-07-02 14:34:18 +0500

    Добавьте файлы проекта.

---
+++ HumanClasses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Создание класса о данных о человеке
+public class Program { 
+    public double Height { get; set; } 
+    public int Age { get; set; }       
+    public double Weight { get; set; } 
+
+    public static List<Program> AllChelovek = new List<Program>();
+
+    public Program(double height, int age, double weight)
+    {
+        Height = height;
+        Age = age;
+        Weight = weight;
+        AllChelovek.Add(this);
+    }
+    //Рассчёт Q исходя из данной по заданию формулы
+    public virtual double CalculateQ()
+    {
+        return (Height * 1.8) - (Age * 4.7) + (Weight * 9.6) + 1255;
+    }
+    //Вывод информации о человеке в консоль
+    public void Print()
+    {
+        Console.WriteLine($"Рост: {Height} см, Возраст: {Age}, Вес: {Weight} кг, Q: {CalculateQ():F2}");
+    }
+    //Удаление людей
+    public static void RemoveHumans(params Program[] humans)
+    {
+        foreach (var h in humans)
+        {
+            AllChelovek.Remove(h);
+        }
+    }
+}
+
+public class Chelovekgoal : Program
+{
+    public string Goal { get; set; }
+
+    public Chelovekgoal(double height, int age, double weight, string goal)
+        : base(height, age, weight)
+    {
+        Goal = goal;
+    }
+    //поиск по fit или fat и либо вычитании из формулы, лиюо добавление
+    public override double CalculateQ()
+    {
+        double baseQ = base.CalculateQ();
+        return Goal == "fit" ? baseQ - 600 : baseQ + 900;
+    }
+    //Вывод в консоль информацию о человеке
+    public new void Print()
+    {
+        Console.WriteLine($"Рост: {Height} см, Возраст: {Age}, Вес: {Weight} кг, Цель: {Goal}, Q: {CalculateQ():F2}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Chelovek 
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа расчета качества человека");
+
+        //Добавление нескольких персон
+        var person1 = new Program(175, 30, 70);
+        var person2 = new Program(160, 25, 55);
+
+       
+        var person3 = new Chelovekgoal(180, 35, 80, "fit");
+        var person4 = new Chelovekgoal(170, 40, 90, "fat");
+
+       //Вывод в консоль всех выше сохданных людей
+        Console.WriteLine("\nВсе люди в системе:");
+        foreach (var h in Program.AllChelovek)
+        {
+            h.Print();
+        }
+
+        //удаление пару персонажей
+        Program.RemoveHumans(person2, person4);
+
+        //Вывод в консоль оставшихся людей после удаления
+        Console.WriteLine("\nОставшиеся люди после удаления:");
+        foreach (var h in Program.AllChelovek)
+        {
+            h.Print();
+            
+        }
+        //Выход из программмы
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
